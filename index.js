c816c8b8090839df6279e473840be1473a4e9fad
harsh <harshdeep1043@gmail.com>
2024-05-29 13:09:26 +0530

    remove vercel.json

--- index.js
+++ index.js
@@ -3,21 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const userRouter=require('./routes/user');
-const authRouter=require("./routes/auth");
-const productRouter=require("./routes/product");
-const cartRouter=require('./routes/cart');
-const orderRouter=require("./routes/order");
+const userRouter = require('./routes/user');
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const cartRouter = require('./routes/cart');
+const orderRouter = require("./routes/order");
 const cors = require('cors');
-const corsConfig={
-  origin:"*",
-  Credential:true,
-  methods:["GET","POST","PUT","DELETE","PATCH"],
-};
-const razorRouter=require("./routes/payment");
 
-app.options("",cors(corsConfig))
-  app.use(cors(corsConfig));
+const razorRouter = require("./routes/payment");
+
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("db connection successful"))
@@ -25,20 +19,29 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
     console.log(err);
   });
 
-app.use(express.json()); //this ensures that json body is read and useful for postman requests
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
-app.use("/api/product",productRouter);
-app.use("/api/order",orderRouter);
-app.use("/api/cart",cartRouter);
-app.use("/api/checkout",razorRouter);
-app.use("/",(req,res)=>{
-  return res.status(200).json(
-    {success:true,
-    message:"Everything fine"}
-  )
-})   
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
-app.listen(process.env.PORT||5000, () => {
+
+app.use(express.json());
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/product", productRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/checkout", razorRouter);
+app.use("/", (req, res) => {
+  return res.status(200).json(
+    {
+      success: true,
+      message: "Everything fine"
+    }
+  )
+})
+
+app.listen(process.env.PORT, () => {
   console.log("backend is running");
 });
